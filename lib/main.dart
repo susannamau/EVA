329cc707f4539001a3965a3350fec06344ec31f1
susannamau <maugeri.susanna@gmail.com>
2023-10-17 16:04:40 +0200

    aggiunta logo in schermata home

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,25 @@ class HomeScreen extends StatefulWidget {
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Welcome to ELBAVIVA'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,  // Centra gli elementi verticalmente nella colonna
+          children: <Widget>[
+            // Scritta centrata
+            Text(
+              'Welcome to ElbaViva',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+
+            // Spaziatura tra la scritta e l'immagine
+            SizedBox(height: 20),  // Puoi regolare l'altezza come preferisci
+
+            // Immagine centrata
+            Image.asset('assets/logo-trasp.png'),
+          ],
+        ),
+      ),
     );
   }
 }
