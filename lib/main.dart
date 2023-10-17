6d69b976f7d3ee2b0d1f5b178e43eba2f2b7c60f
susannamau <maugeri.susanna@gmail.com>
2023-10-17 15:33:41 +0200

    EVA first commit

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ElbaVivaApp());
+
+class ElbaVivaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ELBAVIVA',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Welcome to ELBAVIVA'),
+    );
+  }
+}
+
+class OrariScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Orari'),
+    );
+  }
+}
+
+class VentoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Vento'),
+    );
+  }
+}
+
+class EventiScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Eventi'),
+    );
+  }
+}
+
+class NightLifeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Night Life\n\nqui metteremo la lista delle serate')
+    );
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    WelcomeScreen(),
+    OrariScreen(),
+    VentoScreen(),
+    EventiScreen(),
+    NightLifeScreen(),
+  ];
+  final List<String> _titles = [
+    'Welcome to ELBAVIVA',
+    'Orari',
+    'Vento',
+    'Eventi',
+    'Night Life',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titles[_currentIndex],
+          style: TextStyle(
+          color: Colors.white, //colore del titolo in alto
+          ),
+        ),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.access_time), label: 'Orari'),
+          BottomNavigationBarItem(icon: Icon(Icons.air), label: 'Vento'),
+          BottomNavigationBarItem(icon: Icon(Icons.event), label: 'Eventi'),
+          BottomNavigationBarItem(icon: Icon(Icons.nightlife), label: 'Night Life'),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
