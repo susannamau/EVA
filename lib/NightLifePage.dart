a89ee21b4fc2077867d883e9bead07debeb1f43f
susannamau <maugeri.susanna@gmail.com>
2023-10-28 23:35:17 +0200

    NightLife well formatted

--- lib/NightLifePage.dart
+++ lib/NightLifePage.dart
@@ -1,8 +1,46 @@
 import 'dart:ffi';
-
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'dart:convert';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+
+String printableDay(DateTime d) {
+  switch (d.weekday) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    case 7:
+      return "Sunday";
+    default:
+      return "Invalid day";
+  }
+}
+
+class EventList {
+  final DateTime date;
+  List<Event> eventiDelGiorno;
+  EventList({required this.date, required this.eventiDelGiorno});
+}
+
+class Event {
+  final String locale;
+  final String genere;
+  Event({required this.locale, required this.genere});
+  @override
+  String toString() {
+    return "| $locale, $genere |";
+  }
+}
 
 class NightLife extends StatefulWidget {
   const NightLife({super.key});
@@ -12,13 +50,14 @@ class NightLife extends StatefulWidget {
 }
 
 class _NightLifeState extends State<NightLife> {
-  DatabaseReference database = FirebaseDatabase
-      .instance
-      .ref('serate')
-      .orderByKey()
-      .ref;
+  DatabaseReference database =
+      FirebaseDatabase.instance
+          .ref('serate')
+          .orderByKey()
+          .ref;
   var eventi;
   var data;
+  List<EventList> calendar = [];
 
   @override
   void initState() {
@@ -35,13 +74,24 @@ class _NightLifeState extends State<NightLife> {
       //DatabaseEvent event = await database.child('serate').once(); //in caso si può rimuovere il child
       //DatabaseEvent event = await database.once();
       DataSnapshot data1 = await database.get();
-      print(data1.value);
       setState(() {
         //data = event.snapshot.value;
         data = data1.value;
-        print("tip di data");
-        print(data.runtimeType);
+        //print("tip di data");
+        //print(data.runtimeType);
+        int index = 0;
+        data.forEach((key, value) {
+          DateTime datekey = DateTime.parse(key);
+          calendar.add(EventList(date: datekey, eventiDelGiorno: []));
+          for (var v in value) {
+            calendar[index]
+                .eventiDelGiorno
+                .add(Event(locale: v['locale'], genere: v['genere']));
+          }
+          index++;
+        });
       });
+      calendar.sort((a, b) => a.date.compareTo(b.date));
     } catch (e) {
       print("Errore durante il fetch dei dati: $e");
     }
@@ -75,33 +125,49 @@ class _NightLifeState extends State<NightLife> {
               child: Text("It's cloudy here"),
             ),
             Center(
-              child: data == null ? Text('Non carica') :
-              ListView.builder(
-                itemCount: data.entries.length,
-                itemBuilder: (context, index) {
-                  var date = data.entries.elementAt(index).key;
-                  var events = data[date];
-                  return ListTile(
-                    title: Text(date),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: events.map<Widget>((event) {
-                        return ListTile(
-                          title:Text('Locale: ${event['locale']}'),
-                          subtitle:Text('Genere: ${event['genere']}'),
-                        );
-
-                        //    'Locale: ${event['locale']}, Genere: ${event['genere']}');
-                      }).toList(),
+              child: data == null
+                  ? Text('Non carica')
+                  : CustomScrollView(
+                slivers: calendar.map((eventList) {
+                  return SliverStickyHeader(
+                    header: Container(
+                      height: 50.0,
+                      color: Colors.grey[200],
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      alignment: Alignment.centerLeft,
+                      child: Center (
+                        child: Text(
+                          '${printableDay(eventList.date)} - '
+                            '${eventList.date.day.toString().padLeft(2, '0')}-'
+                                '${eventList.date.month.toString().padLeft(2, '0')}-'
+                                '${eventList.date.year}',
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                      ),
+                    ),
+                    sliver: SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                            (context, i) =>
+                            ListTile(
+                              leading: Icon(Icons.music_note),
+                              title: Text(
+                                  eventList.eventiDelGiorno[i]
+                                      .locale,
+                              ),
+                              subtitle: Text(
+                                  eventList.eventiDelGiorno[i]
+                                      .genere),
+                            ),
+                        childCount: eventList.eventiDelGiorno.length,
+                      ),
                     ),
                   );
-                },
-              )
-
+                }).toList(),
+              ),
             )
           ],
-            ),
         ),
-      );
+      ),
+    );
   }
-  }
+}
